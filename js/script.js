b1176831b2be627b14b9c6073467381d46eb9d13
Julia Missias <juliamissias50@gmail.com>
2024-07-28 18:05:22 -0300

    Atualizações no CSS e JS para melhorar o layout e funcionalidade

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const faders = document.querySelectorAll('.fade-in');
 
+    // Opções do IntersectionObserver
     const appearOptions = {
-        threshold: 0.1,
-        rootMargin: "0px 0px -50px 0px"
+        threshold: 0.1, // Percentual de visibilidade para ativar o efeito
+        rootMargin: "0px 0px -50px 0px" // Margem da área de visualização
     };
 
-    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    // Função de callback para o IntersectionObserver
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
+            if (entry.isIntersecting) {
+                // Adiciona a classe 'appear' se o elemento estiver visível
                 entry.target.classList.add("appear");
-                appearOnScroll.unobserve(entry.target);
+                observer.unobserve(entry.target); // Remove o elemento da observação
             }
         });
     }, appearOptions);
 
+    // Observa cada elemento que deve aparecer com o scroll
     faders.forEach(fader => {
         appearOnScroll.observe(fader);
     });
